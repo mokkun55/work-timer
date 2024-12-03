df8be285d954fb183ab2d7198168b66576ef1547
もっくん <mokkunpc@gmail.com>
2024-12-03 10:44:32 +0900

    add: setup mantineProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import "normalize.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Router } from "./router/Route";
+import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router />
+    <MantineProvider>
+      <Router />
+    </MantineProvider>
   </StrictMode>
 );
