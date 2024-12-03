0270fce8afa193611b78ecc4e99494ef6fe8b277
もっくん <mokkunpc@gmail.com>
2024-12-03 19:00:22 +0900

    feat: baseLayouts

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { Router } from "./router/Route";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import "./global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,81 @@
+import {
+  Button,
+  Center,
+  Container,
+  Flex,
+  Grid,
+  Input,
+  RingProgress,
+} from "@mantine/core";
+
 export const Home = (): React.ReactNode => {
   return (
-    <div>
-      {/* 現在の進捗 */}
+    <Container size="lg" bg="#EAF3FD">
+      <Flex direction="column" gap="md" justify="center" align="center">
+        <Grid grow>
+          {/* 現在の進捗 */}
+          <Flex direction="row">
+            <Grid.Col span={4}>
+              <Flex direction="column" gap="md" justify="center" align="center">
+                <h3>今週の進捗</h3>
+                {/* TODO アニメーションとかいいかも */}
+                <RingProgress
+                  size={300}
+                  thickness={15}
+                  roundCaps
+                  sections={[{ value: 40, color: "blue" }]}
+                  transitionDuration={700}
+                  // taは aligned center
+                  label={
+                    <Center>
+                      <div>
+                        <h1 style={{ margin: "0" }}>40%</h1>
+                        <h1 style={{ margin: "0" }}>5h/16h</h1>
+                        <h1 style={{ margin: "0" }}>あと11h</h1>
+                      </div>
+                    </Center>
+                  }
+                />
+              </Flex>
+            </Grid.Col>
 
-      {/* 記録フォーム */}
+            {/* 最近の記録 */}
+            <Grid.Col span={8}>
+              <h3>今週の記録</h3>
+              <p>11/11(月) : 3h ホームページの制作</p>
+              <p>11/12(月) : 2h コードレビュー</p>
+              <p>11/13(月) : 0.5h デザインの修正</p>
+              <p>11/13(月) : 0.5h デザインの修正</p>
+              <p>11/13(月) : 0.5h デザインの修正</p>
+              <p>11/13(月) : 0.5h デザインの修正</p>
+              <p>11/13(月) : 0.5h デザインの修正</p>
+              <p>11/13(月) : 0.5h デザインの修正</p>
+              <p>11/13(月) : 0.5h デザインの修正</p>
+            </Grid.Col>
+          </Flex>
+        </Grid>
 
-      {/* 最近の記録一覧 */}
+        {/* 記録フォーム */}
+        <div>
+          <Flex direction="column" gap="md" justify="center" align="center">
+            <h3>記録フォーム</h3>
+            {/* TODO 仮置き */}
+            <Input />
+            <Input />
+            <Button>完了</Button>
+            <Button>過去の記録を見る</Button>
+          </Flex>
+        </div>
 
-      {/* 設定画面へ */}
-    </div>
+        {/* 設定画面へ */}
+        <div>
+          <Flex direction="column" gap="md" justify="center" align="center">
+            <Button variant="light" color="gray">
+              設定画面へ
+            </Button>
+          </Flex>
+        </div>
+      </Flex>
+    </Container>
   );
 };
