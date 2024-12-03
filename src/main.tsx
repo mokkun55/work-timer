bf85ea624ac8426a0ab14777aef72bdaca30c308
もっくん <mokkunpc@gmail.com>
2024-12-03 11:16:33 +0900

    add: mantine css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Router } from "./router/Route";
 import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@mantine/core";
+
 export const Home = (): React.ReactNode => {
   return (
     <div>
       <h1>home</h1>
+      <Button>Hello</Button>
     </div>
   );
 };
