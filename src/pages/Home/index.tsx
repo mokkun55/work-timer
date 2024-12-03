8f5389e127f266bfc4497247080afb22c2bc8137
もっくん <mokkunpc@gmail.com>
2024-12-03 19:07:01 +0900

    fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,14 +10,14 @@ import {
 
 export const Home = (): React.ReactNode => {
   return (
-    <Container size="lg" bg="#EAF3FD">
+    <Container size="lg" h="100vh" bg="#EAF3FD">
       <Flex direction="column" gap="md" justify="center" align="center">
         <Grid grow>
           {/* 現在の進捗 */}
           <Flex direction="row">
             <Grid.Col span={4}>
               <Flex direction="column" gap="md" justify="center" align="center">
-                <h3>今週の進捗</h3>
+                <h2>今週の進捗</h2>
                 {/* TODO アニメーションとかいいかも */}
                 <RingProgress
                   size={300}
@@ -41,7 +41,7 @@ export const Home = (): React.ReactNode => {
 
             {/* 最近の記録 */}
             <Grid.Col span={8}>
-              <h3>今週の記録</h3>
+              <h2>今週の記録</h2>
               {/* TODO 縦横 はみ出したら... 表示 */}
               <p>11/11(月) : 3h ホームページの制作</p>
               <p>11/12(月) : 2h コードレビュー</p>
@@ -57,7 +57,7 @@ export const Home = (): React.ReactNode => {
         {/* 記録フォーム */}
         <div>
           <Flex direction="column" gap="md" justify="center" align="center">
-            <h3>記録フォーム</h3>
+            <h2>記録フォーム</h2>
             {/* TODO 仮置き */}
             <Input />
             <Input />
