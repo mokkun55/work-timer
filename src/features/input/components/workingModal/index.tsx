fe11b3e8a6c72b363ade2de4505fdae8a804b8fa
もっくん <mokkunpc@gmail.com>
2024-12-04 09:28:54 +0900

    feat: style workingmodal

--- src/features/input/components/workingModal/index.tsx
+++ src/features/input/components/workingModal/index.tsx
@@ -1,7 +1,10 @@
+import useTimeFormatter from "@/hooks/useTimeFormatter";
 import useTimerStore from "@/store/useTimerStore";
 import useWorkContentStore from "@/store/useWorkContentStore";
 import useWorkStore from "@/store/useWorkStore";
-import { Modal } from "@mantine/core";
+import { Button, Container, Modal, Progress } from "@mantine/core";
+import styles from "./index.module.scss";
+import { MdModeEdit } from "react-icons/md";
 
 type Props = {
   onClose: () => void;
@@ -11,6 +14,7 @@ export const WorkingModal = ({ onClose }: Props): React.ReactNode => {
   const { isWorking } = useWorkStore();
   const { workContent } = useWorkContentStore();
   const { elapsedTime } = useTimerStore();
+  const { formatTime } = useTimeFormatter();
 
   // ページを離れようとすると警告
   if (isWorking) {
@@ -19,12 +23,6 @@ export const WorkingModal = ({ onClose }: Props): React.ReactNode => {
     window.onbeforeunload = null;
   }
 
-  const formatTime = (time: number): string => {
-    const hour = Math.floor(time / 3600);
-    const min = Math.floor((time % 3600) / 60);
-    return `${hour < 10 ? "0" : ""}${hour}:${min < 10 ? "0" : ""}${min}`;
-  };
-
   return (
     <Modal
       opened={isWorking}
@@ -32,14 +30,25 @@ export const WorkingModal = ({ onClose }: Props): React.ReactNode => {
       fullScreen
       withCloseButton={false}
     >
-      <div>
-        <h1>作業中</h1>
-        <p>作業内容: {workContent}</p>
-        <p>作業時間: {formatTime(elapsedTime)}</p>
-        {/* TODO デバック用後で消す */}
-        <p>{elapsedTime}</p>
-        <button onClick={onClose}>終了</button>
-      </div>
+      <Container size="lg" className={styles.container}>
+        <div className={styles.working}>
+          作業中:
+          <div className={styles.inputArea}>
+            <input type="text" value={workContent} className={styles.input} />
+            <MdModeEdit className={styles.icon} />
+          </div>
+        </div>
+        {/* TODO 円の中今はモック */}
+        <div className={styles.progressArea}>
+          <h2 className={styles.title}>今週の進捗</h2>
+          <p className={styles.time}>11h / 16h</p>
+        </div>
+        <Progress value={40} size="xl" radius="xl" />
+        <p className={styles.time}>{formatTime(elapsedTime)}</p>
+        <Button color="red" onClick={onClose}>
+          作業を終了する
+        </Button>
+      </Container>
     </Modal>
   );
 };
