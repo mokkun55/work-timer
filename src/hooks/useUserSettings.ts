d50441e39dbaff6f94971b52359b63ee9c79959d
もっくん <mokkunpc@gmail.com>
2024-12-30 00:00:41 +0900

    [WIP] login feature

--- src/hooks/useUserSettings.ts
+++ src/hooks/useUserSettings.ts
@@ -1,9 +1,34 @@
 import { db } from "@/libs/firebase";
 import { useAuth } from "@/providers/AuthProvider";
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 
 export const useUserSettings = () => {
   const { currentUser } = useAuth();
+
+  // 新規登録の処理
+  const initUserSettings = async (): Promise<void> => {
+    try {
+      if (!currentUser?.uid) {
+        console.error("てすとログインしてください");
+        return;
+      }
+
+      const docRef = doc(db, `users/${currentUser.uid}`);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.error("ユーザー設定がすでに存在します");
+        return;
+      }
+
+      await setDoc(docRef, {
+        goalTime: 8,
+      });
+    } catch (e) {
+      console.error("ユーザー設定の初期化中にエラーが発生しました: ", e);
+    }
+  };
+
   const getGoalTime = async (): Promise<number | undefined> => {
     try {
       if (!currentUser) {
@@ -41,5 +66,5 @@ export const useUserSettings = () => {
       console.error("ユーザー設定の更新中にエラーが発生しました: ", e);
     }
   };
-  return { getGoalTime, updateGoalTime };
+  return { initUserSettings, getGoalTime, updateGoalTime };
 };
--- src/pages/Root/hooks/useSignIn.ts
+++ src/pages/Root/hooks/useSignIn.ts
@@ -1,18 +1,31 @@
+import { useUserSettings } from "@/hooks/useUserSettings";
 import { auth, googleProvider } from "@/libs/firebase";
-import { signInWithPopup, signOut } from "firebase/auth";
+import { getAdditionalUserInfo, signInWithPopup, signOut } from "firebase/auth";
 import toast from "react-hot-toast";
 
 export const useSignIn = () => {
+  const { initUserSettings } = useUserSettings();
+
   // googleでログイン
   const signInWithGoogle = async () => {
     try {
-      await signInWithPopup(auth, googleProvider);
+      const result = await signInWithPopup(auth, googleProvider);
+      const additionalUserInfo = getAdditionalUserInfo(result);
+      if (additionalUserInfo?.isNewUser) {
+        // 新規登録時の処理
+        // TODO 後で消す
+        toast.success("初めてのログインです");
+        await initUserSettings();
+      }
+
       toast.success("ログインしました");
     } catch (e) {
       toast.error("ログインに失敗しました");
       console.log("error", e);
     }
   };
+
+  // TODO const signInWithGuest = async () => {}
 
   const logOut = async () => {
     try {
